34c5321ebc69ae9825e1fc61474952513516d607
Akhil Nerella <92852625+nnv-akhil@users.noreply.github.com>
2022-12-13 22:53:20 +0530

    Delete test case generator.py

--- 1st round/prime-population/test case generator.py
+++
@@ -1,9 +0,0 @@
-import random
-t=random.randint(1,10000)
-print(t)
-while(t):
-    t-=1
-    #print(random.randint(1,100))
-    a=random.randint(1,1000)
-    b=random.randint(a,100000)
-    print(f'{a} {b}')
